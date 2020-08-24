8809a4b4ac478aa49bdeaa6a856d4709a42c01f5
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-08-24 17:31:18 +0100

    Added plural phrase in header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import WeeklyBasket from "./components/WeeklyBasket";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
 
       <main role="main" className="pt-5 px-3">
-        <h1 className="is-size-4 has-text-centered mb-4">
-          In this Week's Grootbasket — 17 Aug 2020
-        </h1>
-
+        <Header />
         <WeeklyBasket />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="title is-4 has-text-centered mb-5">
+        In this Week's Grootbasket — 17 Aug 2020
+      </h1>
+
+      <p>2,342 baskets delivered</p>
+    </div>
+  );
+}
+
+export default Header;
