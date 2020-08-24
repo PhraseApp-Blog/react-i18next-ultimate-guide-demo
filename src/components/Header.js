72be95b2f9817bf7b90a388f9e713af20169a19f
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-08-24 17:43:00 +0100

    Refactoring Header to class-based component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function Header() {
-  return (
-    <div className="header">
-      <h1 className="title is-4 has-text-centered mb-5">
-        In this Week's Grootbasket — 17 Aug 2020
-      </h1>
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1 className="title is-4 has-text-centered mb-5">
+          In this Week's Grootbasket — 17 Aug 2020
+        </h1>
 
-      <p>2,342 baskets delivered</p>
-    </div>
-  );
+        <p>2,342 baskets delivered</p>
+      </div>
+    );
+  }
 }
 
 export default Header;
