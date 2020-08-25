1fe475a63fbc8a81316f1f4ea025a9182a2dcb66
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-08-25 20:06:43 +0100

    Adding browser locale auto-detection

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,6 +1,8 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
 import {
   defaultLanguage,
   supportedLanguages,
@@ -9,12 +11,15 @@ import {
 i18next
   .use(initReactI18next)
   .use(HttpApi)
+  .use(LanguageDetector)
   .init({
-    lng: defaultLanguage,
+    // lng will override the browser detector if provided
+    // lng: defaultLanguage,
     supportedLngs: supportedLanguages.map(
       (lang) => lang.code,
     ),
     fallbackLng: defaultLanguage,
+    // eagerly loaded namespaces
     // ns: ["translation", "navbar"],
     backend: {
       loadPath: "/lang/{{lng}}/{{ns}}.json",
