92b360bf807c3dabe92da869862f269de925eaed
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-09-22 15:49:05 +0100

    Adding customizable date formatting

--- src/services/i18n-format.js
+++ src/services/i18n-format.js
@@ -2,11 +2,44 @@ function format(value, format, lng) {
   if (value instanceof Date) {
     return formatDate(value, format, lng);
   }
+
   return value;
 }
 
 function formatDate(value, format, lng) {
-  return new Intl.DateTimeFormat(lng).format(value);
+  const options = dateOptions(format);
+
+  return options === null
+    ? value
+    : new Intl.DateTimeFormat(lng, options).format(value);
+}
+
+function dateOptions(format) {
+  if (format === "default") {
+    return {};
+  } else {
+    try {
+      return JSON.parse(jsonCompliant(format));
+    } catch (err) {
+      console.error(err);
+
+      return null;
+    }
+  }
+}
+
+function jsonCompliant(format) {
+  const namesWrappedWithDoubleQuotes = format
+    .split(",")
+    .map((param) =>
+      param
+        .split(":")
+        .map((name) => `"${name.trim()}"`)
+        .join(":"),
+    )
+    .join(",");
+
+  return `{${namesWrappedWithDoubleQuotes}}`;
 }
 
 export default format;
